7cddc3cd4f1771a1c3713906e13ef8dd04c344d7
mikaeltyrsen <mikael@nucontext.com>
2025-11-25 05:45:49 -0800

    Use HTTP base URL for device builds

--- Noise/Networking/APIClient.swift
+++ Noise/Networking/APIClient.swift
@@ -5,7 +5,7 @@ enum APIEnvironment {
         #if targetEnvironment(simulator)
         return URL(string: "http://dev.local.makenoise.app/api/v1/")!
         #else
-        return URL(string: "https://makenoise.app/api/v1/")!
+        return URL(string: "http://makenoise.app/api/v1/")!
         #endif
     }
 }
